20da60aa27ec0901547c8d159cee1e1f5331384f
Hindya Pwayari Razzaq <hindyapr@gmail.com>
2019-04-01 20:44:19 +0700

    Edited

--- config/config.js
+++ config/config.js
@@ -1,18 +1,4 @@
 module.exports = {
-    "development": {
-        "username": `${process.env.DB_USERNAME}`,
-        "password": `${process.env.DB_PASSWORD}`,
-        "database": "rest_api",
-        "host": "127.0.0.1",
-        "dialect": "postgres"
-    },
-    "test": {
-        "username": `${process.env.DB_USERNAME}`,
-        "password": `${process.env.DB_PASSWORD}`,
-        "database": "rest_api",
-        "host": "127.0.0.1",
-        "dialect": "postgres"
-    },
     "production": {
         "username": "xrfmfffthfbmpy",
         "password": "fd9dc7610b184ad5724cea08f8d8edeef72a3411ee9cf478d48d2062a169f026",
