bcc88b3e50f094dd8125cb755f5b263b3344fe3d
Hindya Pwayari Razzaq <hindyapr@gmail.com>
2019-04-01 20:27:20 +0700

    Edited

--- config/config.js
+++ config/config.js
@@ -17,7 +17,7 @@ module.exports = {
         "username": `xrfmfffthfbmpy`,
         "password": `fd9dc7610b184ad5724cea08f8d8edeef72a3411ee9cf478d48d2062a169f026`,
         "database": "d94of7ch93hm64",
-        "host": "127.0.0.1",
+        "host": "ec2-54-225-242-183.compute-1.amazonaws.com",
         "dialect": "postgres"
     }
 }
