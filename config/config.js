96250a90ce2ae681163ee1c7ccc1b7e7c0cb83d8
Hindya Pwayari Razzaq <hindyapr@gmail.com>
2019-04-01 21:05:11 +0700

    Edited

--- config/config.js
+++ config/config.js
@@ -1,7 +1,14 @@
 module.exports = {
+    "development": {
+        "username": process.env.DB_USERNAME,
+        "password": process.env.DB_PASSWORD,
+        "database": "d94of7ch93hm64",
+        "host": "ec2-54-225-242-183.compute-1.amazonaws.com",
+        "dialect": "postgres"
+    },
     "production": {
-        "username": "xrfmfffthfbmpy",
-        "password": "fd9dc7610b184ad5724cea08f8d8edeef72a3411ee9cf478d48d2062a169f026",
+        "username": process.env.DB_USERNAME,
+        "password": process.env.DB_PASSWORD,
         "database": "d94of7ch93hm64",
         "host": "ec2-54-225-242-183.compute-1.amazonaws.com",
         "dialect": "postgres"
